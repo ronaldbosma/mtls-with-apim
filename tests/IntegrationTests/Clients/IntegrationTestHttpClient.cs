059ff83b611c8bf0dbabf7d03b442fe6cab5c2c3
Ronald Bosma <ronaldbosma@outlook.com>
2026-04-17 15:01:53 +0200

    Add tests that use valid and invalid client cert to call operations on protected api

--- tests/IntegrationTests/Clients/IntegrationTestHttpClient.cs
+++ tests/IntegrationTests/Clients/IntegrationTestHttpClient.cs
@@ -1,22 +1,42 @@
+using System.Security.Cryptography.X509Certificates;
+
 using IntegrationTests.Clients.Handlers;
 
-namespace IntegrationTests.Clients
+namespace IntegrationTests.Clients;
+
+/// <summary>
+/// Represents an HTTP client used for integration testing, configured with a base address 
+/// and a custom message handler for logging HTTP requests and responses.
+/// Optionally, it can be configured to use a client certificate for authentication.
+/// </summary>
+internal class IntegrationTestHttpClient : HttpClient
 {
     /// <summary>
-    /// Represents an HTTP client used for integration testing, configured with a base address 
-    /// and a custom message handler for logging HTTP requests and responses.
+    /// Initializes a new instance of the <see cref="IntegrationTestHttpClient"/> class with the specified base address.
+    /// No authentication is configured for this client.
     /// </summary>
-    internal class IntegrationTestHttpClient : HttpClient
+    /// <param name="baseAddress">The base address of the HTTP client.</param>
+    public IntegrationTestHttpClient(Uri baseAddress)
+        : base(new HttpMessageLoggingHandler(new HttpClientHandler()))
     {
-        public IntegrationTestHttpClient(string baseAddress)
-            : this(new Uri(baseAddress))
-        {
-        }
+        BaseAddress = baseAddress;
+    }
 
-        public IntegrationTestHttpClient(Uri baseAddress)
-            : base(new HttpMessageLoggingHandler(new HttpClientHandler()))
-        {
-            BaseAddress = baseAddress;
-        }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IntegrationTestHttpClient"/> class with the specified base address and client certificate.
+    /// </summary>
+    /// <param name="baseAddress">The base address of the HTTP client.</param>
+    /// <param name="clientCertificate">The client certificate to use for authentication.</param>
+    public IntegrationTestHttpClient(Uri baseAddress, X509Certificate2 clientCertificate)
+        : base(CreateHandlerWithCertificate(clientCertificate))
+    {
+        BaseAddress = baseAddress;
+    }
+
+    private static HttpMessageLoggingHandler CreateHandlerWithCertificate(X509Certificate2 certificate)
+    {
+        var handler = new HttpClientHandler();
+        handler.ClientCertificates.Add(certificate);
+        return new HttpMessageLoggingHandler(handler);
     }
 }
--- tests/IntegrationTests/ProtectedApiTests.cs
+++ tests/IntegrationTests/ProtectedApiTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
 using IntegrationTests.Clients;
 using IntegrationTests.Configuration;
@@ -8,6 +9,24 @@ namespace IntegrationTests;
 [TestClass]
 public sealed class ProtectedTests
 {
+    private static readonly X509Certificate2 ValidClientCertificate = X509CertificateLoader.LoadPkcs12FromFile(@"C:\repos\ronaldbosma\mtls-with-apim\self-signed-certificates\certificates\dev-client-01.pfx", "P@ssw0rd");
+    private static readonly X509Certificate2 InvalidClientCertificate = X509CertificateLoader.LoadPkcs12FromFile(@"C:\repos\ronaldbosma\mtls-with-apim\self-signed-certificates\certificates\tst-client-01.pfx", "P@ssw0rd");
+
+    [TestMethod]
+    public async Task ValidateUsingPolicy_ValidClientCertificateProvided_200OkReturned()
+    {
+        // Arrange
+        var config = TestConfiguration.Load();
+
+        using var apimClient = new IntegrationTestHttpClient(config.AzureApiManagementGatewayUrl, ValidClientCertificate);
+
+        // Act
+        var response = await apimClient.GetAsync("protected/validate-using-policy");
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
     [TestMethod]
     public async Task ValidateUsingPolicy_NoClientCertificateProvided_401UnauthorizedReturned()
     {
@@ -24,12 +43,57 @@ public sealed class ProtectedTests
     }
 
     [TestMethod]
+    public async Task ValidateUsingPolicy_InvalidClientCertificateProvided_401UnauthorizedReturned()
+    {
+        // Arrange
+        var config = TestConfiguration.Load();
+
+        using var apimClient = new IntegrationTestHttpClient(config.AzureApiManagementGatewayUrl, InvalidClientCertificate);
+
+        // Act
+        var response = await apimClient.GetAsync("protected/validate-using-policy");
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task ValidateUsingContext_ValidClientCertificateProvided_200OkReturned()
+    {
+        // Arrange
+        var config = TestConfiguration.Load();
+
+        using var apimClient = new IntegrationTestHttpClient(config.AzureApiManagementGatewayUrl, ValidClientCertificate);
+
+        // Act
+        var response = await apimClient.GetAsync("protected/validate-using-context");
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [TestMethod]
     public async Task ValidateUsingContext_NoClientCertificateProvided_401UnauthorizedReturned()
     {
         // Arrange
         var config = TestConfiguration.Load();
 
         using var apimClient = new IntegrationTestHttpClient(config.AzureApiManagementGatewayUrl);
+
+        // Act
+        var response = await apimClient.GetAsync("protected/validate-using-context");
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task ValidateUsingContext_InvalidClientCertificateProvided_401UnauthorizedReturned()
+    {
+        // Arrange
+        var config = TestConfiguration.Load();
+
+        using var apimClient = new IntegrationTestHttpClient(config.AzureApiManagementGatewayUrl, InvalidClientCertificate);
 
         // Act
         var response = await apimClient.GetAsync("protected/validate-using-context");
