2fb7ab63a6827c3f363357e6b65060a46c69694b
Ronald Bosma <ronaldbosma@outlook.com>
2026-05-13 18:39:01 +0200

    Update certificates to include a cert that is not yet valid

--- self-signed-certificates/generate-client-certificates.ps1
+++ self-signed-certificates/generate-client-certificates.ps1
@@ -26,21 +26,24 @@ $certificateTree = @{
             Id              = "dev-intermediate-ca"
             ExpiresInMonths = $CertificateExpirationInMonths
             Clients         = @(
-                @{ Subject = "CN=Client 01"; Id = "dev-client-01"; DnsName = "Client 01"; ExpiresInMonths = $CertificateExpirationInMonths },
-                @{ Subject = "CN=Client 02"; Id = "dev-client-02"; DnsName = "Client 02"; ExpiresInMonths = $CertificateExpirationInMonths },
-                @{ Subject = "CN=Unprotected API"; Id = "dev-unprotected-api"; DnsName = "Unprotected API"; ExpiresInMonths = $CertificateExpirationInMonths },
-                @{ Subject = "CN=Integration Tests"; Id = "dev-integration-tests"; DnsName = "Integration Tests"; ExpiresInMonths = $CertificateExpirationInMonths },
-                # We can't actually create an expired certificate, but we can set the expiration date to the current date so it expires tomorrow
-                @{ Subject = "CN=Expired"; Id = "dev-expired"; DnsName = "Expired"; ExpiresInMonths = 0 }
+                @{ Subject = "CN=Client 01"; Id = "dev-client-01"; DnsName = "Client 01"; StartsAfterMonths = 0; ExpiresInMonths = $CertificateExpirationInMonths },
+                @{ Subject = "CN=Client 02"; Id = "dev-client-02"; DnsName = "Client 02"; StartsAfterMonths = 0; ExpiresInMonths = $CertificateExpirationInMonths },
+                @{ Subject = "CN=Unprotected API"; Id = "dev-unprotected-api"; DnsName = "Unprotected API"; StartsAfterMonths = 0; ExpiresInMonths = $CertificateExpirationInMonths },
+                @{ Subject = "CN=Integration Tests"; Id = "dev-integration-tests"; DnsName = "Integration Tests"; StartsAfterMonths = 0; ExpiresInMonths = $CertificateExpirationInMonths },
+
+                # Create a client certificate that is not yet valid by setting the NotBefore date in the future. This can be used to test the NotBefore validation in APIM.
+                @{ Subject = "CN=NotYetValid"; Id = "dev-notyetvalid"; DnsName = "NotYetValid"; StartsAfterMonths = $CertificateExpirationInMonths-1; ExpiresInMonths = $CertificateExpirationInMonths }
+                # Create an expired client certificate by setting the NotAfter date in the past. This can be used to test the NotAfter validation in APIM.
+                @{ Subject = "CN=Expired"; Id = "dev-expired"; DnsName = "Expired"; StartsAfterMonths = 0; ExpiresInMonths = 0 }
             )
         },
         @{
             Subject         = "CN=APIM Sample TST Intermediate CA"
             Id              = "tst-intermediate-ca"
-            ExpiresInMonths = $CertificateExpirationInMonths
+            StartsAfterMonths = 0; ExpiresInMonths = $CertificateExpirationInMonths
             Clients         = @(
-                @{ Subject = "CN=Client 01"; Id = "tst-client-01"; DnsName = "Client 01"; ExpiresInMonths = $CertificateExpirationInMonths },
-                @{ Subject = "CN=Client 02"; Id = "tst-client-02"; DnsName = "Client 02"; ExpiresInMonths = $CertificateExpirationInMonths }
+                @{ Subject = "CN=Client 01"; Id = "tst-client-01"; DnsName = "Client 01"; StartsAfterMonths = 0; ExpiresInMonths = $CertificateExpirationInMonths },
+                @{ Subject = "CN=Client 02"; Id = "tst-client-02"; DnsName = "Client 02"; StartsAfterMonths = 0; ExpiresInMonths = $CertificateExpirationInMonths }
             )
         }
     )
@@ -69,7 +72,7 @@ foreach ($intermediate in $certificateTree.Intermediates) {
 
     foreach ($client in $intermediate.Clients) {
         $dnsName = if ($client.ContainsKey('DnsName')) { $client.DnsName } else { $client.Subject }
-        $clientCert = New-SelfSignedClientCertificate -Subject $client.Subject -DnsName $dnsName -Signer $intermediateCert -ExpiresInMonths $client.ExpiresInMonths
+        $clientCert = New-SelfSignedClientCertificate -Subject $client.Subject -DnsName $dnsName -Signer $intermediateCert -ExpiresInMonths $client.ExpiresInMonths -StartsAfterMonths $client.StartsAfterMonths
         $clientCerts[$client.Id] = $clientCert
     }
 }
