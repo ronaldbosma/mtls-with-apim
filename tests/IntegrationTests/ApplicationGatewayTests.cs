5f1ef338947e5f15367132015eea23ba6d062c88
Ronald Bosma <ronaldbosma@outlook.com>
2026-05-15 17:48:27 +0200

    Add tests that verify calls to validate-using-context and validate-using-policy on the Protected API fail, even if a valid cert is provided

--- tests/IntegrationTests/ApplicationGatewayTests.cs
+++ tests/IntegrationTests/ApplicationGatewayTests.cs
@@ -69,4 +69,41 @@ public class ApplicationGatewayTests
         // Assert
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
     }
+
+
+
+    /// <summary>
+    /// Even though a valid client certificate is provided, a 401 Unauthorized response is expected because the Application Gateway terminates the TLS connection.
+    /// The client certificate is never forwarded to the backend, so APIM has no way to validate it via the validate-client-certificate policy.
+    /// </summary>
+    [TestMethod]
+    public async Task ValidateUsingPolicy_AgwMtlsEndpoint_ValidClientCertificate_401UnauthorizedReturned()
+    {
+        // Arrange
+        using var agwClient = new IntegrationTestHttpClient(Config.ApplicationGatewayIpAddress!, 53029, Config.ApplicationGatewayHostname!, s_validClientCertificate);
+        
+        // Act
+        var response = await agwClient.GetAsync("protected/validate-using-policy");
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    /// <summary>
+    /// Even though a valid client certificate is provided, a 401 Unauthorized response is expected because the Application Gateway terminates the TLS connection.
+    /// The client certificate is never forwarded to the backend, so APIM has no way to validate it via the context.Request.Certificate property.
+    /// </summary>
+    [TestMethod]
+    public async Task ValidateUsingContext_AgwMtlsEndpoint_ValidClientCertificate_401UnauthorizedReturned()
+    {
+        // Arrange
+        using var agwClient = new IntegrationTestHttpClient(Config.ApplicationGatewayIpAddress!, 53029, Config.ApplicationGatewayHostname!, s_validClientCertificate);
+
+        // Act
+        var response = await agwClient.GetAsync("protected/validate-using-context");
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
 }
